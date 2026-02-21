92d4cde343ca8ee401c41f678db7f3a0481f04f3
indianathe3rd <indiemenziwa@gmail.com>
2026-02-21 17:37:52 +0200

    connect WeatherRepositoryImpl to WeatherApiService

--- app/src/main/java/com/example/bikeweatherforecastapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/bikeweatherforecastapp/data/repository/WeatherRepositoryImpl.kt
@@ -1,8 +1,22 @@
 package com.example.bikeweatherforecastapp.data.repository
 
+import com.example.bikeweatherforecastapp.data.remote.Config
 import com.example.bikeweatherforecastapp.data.remote.WeatherApiService
+import com.example.bikeweatherforecastapp.domain.model.WeatherResponse
+import com.example.bikeweatherforecastapp.domain.repository.WeatherRepository
 
 class WeatherRepositoryImpl(
     private val apiService: WeatherApiService,
-) {
+) : WeatherRepository{
+    override suspend fun getWeatherForecast(
+        lat: Double,
+        lon: Double
+    ): Result<WeatherResponse> {
+        return try {
+            val response = apiService.getWeatherForecast(lat,lon, Config.OPENWEATHER_API_KEY)
+            Result.success(response)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
 }
--- app/src/main/java/com/example/bikeweatherforecastapp/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/bikeweatherforecastapp/domain/repository/WeatherRepository.kt
@@ -1,5 +1,7 @@
 package com.example.bikeweatherforecastapp.domain.repository
 
+import com.example.bikeweatherforecastapp.domain.model.WeatherResponse
+
 interface WeatherRepository {
-    suspend fun getWeatherForecast(lat: Double, lon: Double): Result<WeatherRepository>
+    suspend fun getWeatherForecast(lat: Double, lon: Double): Result<WeatherResponse>
 }
