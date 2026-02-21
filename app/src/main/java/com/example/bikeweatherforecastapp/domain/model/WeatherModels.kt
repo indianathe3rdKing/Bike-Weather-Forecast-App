45961968b9d619bf1d3b3142871c30a774a9d050
indianathe3rd <indiemenziwa@gmail.com>
2026-02-21 12:33:52 +0200

    define weather models

---
+++ app/src/main/java/com/example/bikeweatherforecastapp/domain/model/WeatherModels.kt
@@ -0,0 +1,76 @@
+package com.example.bikeweatherforecastapp.domain.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherResponse(
+    val city: City,
+    val list: List<WeatherItem>,
+    val daily: List<DailyForestcast> = emptyList()
+)
+
+data class City(
+    val id: Int,
+    val name: String,
+    val country: String,
+    val coord: Coordinates
+)
+
+data class Coordinates(
+    val lat: Double,
+    val lon: Double
+)
+
+data class WeatherItem(
+    @SerializedName("dt")
+    val date: Long,
+    val main: MainWeather,
+    val weather: List<Weather>,
+    val wind: Wind,
+    @SerializedName("pop")
+    val precipitationPredictability: Double
+)
+
+data class MainWeather(
+    @SerializedName("temp")
+    val temp: Double,
+    @SerializedName("temp_min")
+    val tempMin: Double,
+    @SerializedName("temp_max")
+    val tempMax: Double,
+    val humidity: Int
+)
+
+data class Weather(
+    val id: Int,
+    val main: String,
+    val description: String,
+    val icon: String
+)
+
+data class Wind(
+    val speed: Double
+)
+
+data class DailyForestcast(
+    val date:Long,
+    val temperature: Temperature,
+    val weather: List<Weather>,
+    val humidity: Int,
+    val windSpeed: Double,
+    val precipitationPredictability: Double
+)
+
+data class Temperature(
+    val day: Double,
+    val min: Double,
+    val max: Double,
+    val night: Double
+)
+
+data class WeatherState(
+    val isLoading: Boolean= false,
+    val weatherData: WeatherResponse?= null,
+    val error: String?=null
+)
+
